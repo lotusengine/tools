f8364905d430a959d9aef6d193885c22e2e21340
cyberwombat <yashua@urbanlotus.com>
2022-09-08 07:41:11 -0700

    Refactor

--- packages/types/src/types/collection.ts
+++ packages/types/src/types/collection.ts
@@ -67,8 +67,30 @@ export interface CollectionMapping {
 }
 
 export interface CollectionProjection {
-  list?: Array<CollectionProjectionParams>
-  detail?: Array<CollectionProjectionParams>
+  list?: Projection
+  detail?: Projection
+}
+
+export type Projection = Array<ProjectionField>
+
+export type ProjectionField = ProjectionFieldText | ProjectionFieldList
+
+export enum ProjectionFieldType {
+  TEXT = 'text',
+  LIST = 'list'
+}
+
+export interface ProjectionFieldCommon {
+  path: string
+  label: string
+}
+
+export interface ProjectionFieldText extends ProjectionFieldCommon {
+  type: `${ProjectionFieldType.TEXT}`
+}
+
+export interface ProjectionFieldList extends ProjectionFieldCommon {
+  type: `${ProjectionFieldType.LIST}`
 }
 
 export interface CollectionProjectionParams {
